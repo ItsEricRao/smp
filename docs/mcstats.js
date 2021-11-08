2ae1fdd18c8cf0b3143008e75f54f61b8823d2f2
EricRao_ <49305406+ItsEricRao@users.noreply.github.com>
2021-11-08 13:09:09 +0800

    Update mcstats.js

--- docs/mcstats.js
+++ docs/mcstats.js
@@ -12,7 +12,7 @@ function fetchData() {
 		const players_online = data.players_online
 		const version = data.version
 		document.open();
-		document.write("<h1>-=EricRao SMP服务器状态=-</h1>");
+		document.write("<center><h1>-=EricRao SMP服务器状态=-</h1>");
 		document.write("<a>状态：</a>");
 		document.write(status);
 		document.write("<br><a>在线玩家：</a>");
@@ -21,7 +21,7 @@ function fetchData() {
 		document.write(players_max);
 		document.write("<br><a>游戏版本：</a>");
 		document.write(version);
-		document.write("<br><ul><li>聊天室：<span id=\"ping-chat\"><\/span><\/li><li>官网：<span id=\"ping-web\"><\/span><\/li><li>文档：<span id=\"ping-doc\"><\/span><\/li><li>Dynmap地图：<span id=\"ping-map\"><\/span><\/li><\/ul>");
+                document.write("</center>");
 		document.close();
 
 		
@@ -34,78 +34,3 @@ function fetchData() {
 }
 
 fetchData();
-
-/* ping.js - v0.2.2 http://github.com/alfg/ping.js */
-var Ping = function(a) {
-    this.opt = a || {},
-    this.favicon = this.opt.favicon || "/favicon.ico",
-    this.timeout = this.opt.timeout || 0,
-    this.logError = this.opt.logError || !1
-};
-Ping.prototype.ping = function(a, b) {
-    function c(a) {
-        f.wasSuccess = !0,
-        e.call(f, a)
-    }
-    function d(a) {
-        f.wasSuccess = !1,
-        e.call(f, a)
-    }
-    function e() {
-        g && clearTimeout(g);
-        var a = new Date - h;
-        if ("function" == typeof b)
-            return this.wasSuccess ? b(null, a) : (f.logError && console.error("error loading resource"),
-            b("error", a))
-    }
-    var f = this;
-    f.wasSuccess = !1,
-    f.img = new Image,
-    f.img.onload = c,
-    f.img.onerror = d;
-    var g, h = new Date;
-    f.timeout && (g = setTimeout(function() {
-        e.call(f, void 0)
-    }, f.timeout)),
-    f.img.src = a + f.favicon + "?" + +new Date
-}
-,
-"undefined" != typeof exports ? "undefined" != typeof module && module.exports && (module.exports = Ping) : window.Ping = Ping;
-
-
-var p = new Ping();
- 
-
-p.ping("http://chat.ericrao.icu", function(err, data) {
-  // Also display error if err is returned.
-  if (err) {
-    console.log("error loading resource")
-    data = data + " " + err;
-  }
-  document.getElementById("ping-chat").innerHTML = data;
-});
-
-p.ping("http://smp.ericrao.icu", function(err, data) {
-  if (err) {
-    console.log("error loading resource")
-    data = data + " " + err;
-  }
-  document.getElementById("ping-web").innerHTML = data;
-});
-
-p.ping("https://docs.ericrao.icu", function(err, data) {
-  if (err) {
-    console.log("error loading resource")
-    data = data + " " + err;
-  }
-  document.getElementById("ping-doc").innerHTML = data;
-});
-
-
-p.ping("http://map.ericrao.icu", function(err, data) {
-  if (err) {
-    console.log("error loading resource")
-    data = data + " " + err;
-  }
-  document.getElementById("ping-map").innerHTML = data;
-});
